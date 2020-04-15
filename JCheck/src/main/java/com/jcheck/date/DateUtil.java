3cb48fe69d31938c193c204ef1b22ba1617ffe75
“赵田雨 <428064270@qq.com>
2020-04-15 10:31:14 +0800

    增加依照今天日期获取当天、周、月、年的方法

--- JCheck/src/main/java/com/jcheck/date/DateUtil.java
+++ JCheck/src/main/java/com/jcheck/date/DateUtil.java
@@ -2,6 +2,7 @@ package com.jcheck.date;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -10,15 +11,111 @@ import java.util.Date;
 public class DateUtil {
 
     //禁止实例化
-    private DateUtil(){}
+    private DateUtil() {
+    }
 
-    public static String dateToString(Date date,String format){
-        SimpleDateFormat sdf =new SimpleDateFormat(format);
+    public static String dateToString(Date date, String format) {
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
         return sdf.format(date);
     }
 
-    public static Date stringToDate(String dateString,String format) throws ParseException {
-        SimpleDateFormat sdf =new SimpleDateFormat(format);
+    public static Date stringToDate(String dateString, String format) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
         return sdf.parse(dateString);
     }
+
+    /**
+     * 获取今天
+     *
+     * @return String
+     */
+    public static String getToday() {
+        return new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+    }
+
+    /**
+     * 获取昨天
+     *
+     * @return String
+     */
+    public static String getYestoday() {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, -1);
+        Date time = cal.getTime();
+        return new SimpleDateFormat("yyyy-MM-dd").format(time);
+    }
+
+    /**
+     * 获取本月开始日期
+     *
+     * @return String
+     **/
+    public static String getMonthStart() {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.MONTH, 0);
+        cal.set(Calendar.DAY_OF_MONTH, 1);
+        Date time = cal.getTime();
+        return new SimpleDateFormat("yyyy-MM-dd").format(time);
+    }
+
+    /**
+     * 获取本月最后一天
+     *
+     * @return String
+     **/
+    public static String getMonthEnd() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.DAY_OF_MONTH, cal.getActualMaximum(Calendar.DAY_OF_MONTH));
+        Date time = cal.getTime();
+        return new SimpleDateFormat("yyyy-MM-dd").format(time);
+    }
+
+    /**
+     * 获取本周的第一天
+     *
+     * @return String
+     **/
+    public static String getWeekStart() {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.WEEK_OF_MONTH, 0);
+        cal.set(Calendar.DAY_OF_WEEK, 2);
+        Date time = cal.getTime();
+        return new SimpleDateFormat("yyyy-MM-dd").format(time);
+    }
+
+    /**
+     * 获取本周的最后一天
+     *
+     * @return String
+     **/
+    public static String getWeekEnd() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.DAY_OF_WEEK, cal.getActualMaximum(Calendar.DAY_OF_WEEK));
+        cal.add(Calendar.DAY_OF_WEEK, 1);
+        Date time = cal.getTime();
+        return new SimpleDateFormat("yyyy-MM-dd").format(time);
+    }
+
+    /**
+     * 获取本年的第一天
+     *
+     * @return String
+     **/
+    public static String getYearStart() {
+        return new SimpleDateFormat("yyyy").format(new Date()) + "-01-01";
+    }
+
+    /**
+     * 获取本年的最后一天
+     *
+     * @return String
+     **/
+    public static String getYearEnd() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.MONTH, calendar.getActualMaximum(Calendar.MONTH));
+        calendar.set(Calendar.DAY_OF_MONTH, calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+        Date currYearLast = calendar.getTime();
+        return new SimpleDateFormat("yyyy-MM-dd").format(currYearLast);
+    }
+
 }
